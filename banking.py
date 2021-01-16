21d92930defe232f250d7b5427b072fe1e10d459
Yurii Syrotenko <crimsonland8@gmail.com>
2021-01-16 17:32:07 +0200

    Refactoring. Moved all the logic to the class.

--- banking.py
+++ banking.py
@@ -6,71 +6,70 @@ class BankAccount:
         self._customer_number = None
         self.card_number = None
         self.card_pin = None
+        self.entered_card_number = None
+        self.entered_card_pin = None
+        self.answer = None
         self.balance = 0
 
-    def print_banner(self):
-        print("""
-1. Create an account
-2. Log into account
-0. Exit
-    """)
+    @staticmethod
+    def print_main_banner():
+        print("1. Create an account\n2. Log into account\n0. Exit")
 
-    def print_banner_second_level(self):
-        print("""
-1. Balance
-2. Log out
-0. Exit
-    """)
-
-    def create_customer_number(self):
-        self.customer_number = str(random.randint(1, 999999999)).zfill(9)
-        return self.customer_number
+    @staticmethod
+    def print_second_banner():
+        print("1. Balance\n2. Log out\n0. Exit")
 
     def create_card_number(self):
-        self.card_number = "400000" + self.create_customer_number() + "8"
-        return self.card_number
+        self._customer_number = str(random.randint(1, 999999999)).zfill(9)
+        self.card_number = "400000" + self._customer_number + str(random.randint(1, 9))
 
     def create_card_pin(self):
         self.card_pin = str(random.randint(0, 9999)).zfill(4)
-        return self.card_pin
+
+    def enter_card_details(self):
+        print("Enter your card number:")
+        self.entered_card_number = str(input())
+        print("Enter your PIN:")
+        self.entered_card_pin = str(input())
+
+    def main_menu(self):
+        self.print_main_banner()
+        self.answer = int(input())
+
+        if self.answer == 1:
+            self.create_card_number()
+            self.create_card_pin()
+            print("Your card has been created")
+            print(f"Your card number:\n{self.card_number}")
+            print(f"Your card PIN:\n{self.card_pin}")
+            self.main_menu()
+        elif self.answer == 2:
+            self.enter_card_details()
+            if self.entered_card_number == self.card_number and self.entered_card_pin == self.card_pin:
+                print("You have successfully logged in!")
+                self.account_menu()
+            else:
+                print("Wrong card number or PIN!")
+                self.main_menu()
+        elif self.answer == 0:
+            print("Bye!")
+        else:
+            self.main_menu()
+
+    def account_menu(self):
+        self.print_second_banner()
+        self.answer = int(input())
+        if self.answer == 1:
+            print(f"Balance: {self.balance}")
+            self.account_menu()
+        elif self.answer == 2:
+            print("You have successfully logged out!")
+            self.main_menu()
+        elif self.answer == 0:
+            print("Bye!")
+        else:
+            self.account_menu()
 
 
 bank_account = BankAccount()
-
-while True:
-    breaker = False
-    bank_account.print_banner()
-    answer = int(input())
-
-    if answer == 1:
-        card_number = bank_account.create_card_number()
-        pin_number = bank_account.create_card_pin()
-        print("Your card has been created")
-        print(f"Your card number:\n{card_number}")
-        print(f"Your card PIN:\n{pin_number}")
-        continue
-    if answer == 2:
-        print("Enter your card number:")
-        entered_card = str(input())
-        print("Enter your PIN:")
-        entered_pin = str(input())
-        if entered_card != card_number or entered_pin != pin_number:
-            print("Wrong card number or PIN!")
-            continue
-        else:
-            print("You have successfully logged in!")
-            while True:
-                bank_account.print_banner_second_level()
-                answer_second_level = int(input())
-                if answer_second_level == 1:
-                    print(bank_account.balance)
-                    continue
-                if answer_second_level == 2:
-                    print("You have successfully logged out!")
-                    break
-                if answer_second_level == 0:
-                    breaker = True
-                    break
-    if answer == 0 or breaker == True:
-        print("Bye!")
-        break
+bank_account.main_menu()
